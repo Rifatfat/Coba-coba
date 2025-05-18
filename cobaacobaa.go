62b9308eea220ff43759068bfd2c1cff30994a06
Rifat <rifatwicaksono17@gmail.com>
2025-05-18 14:49:24 +0700

    nambah hapus

--- cobaacobaa.go
+++ cobaacobaa.go
@@ -121,4 +121,35 @@ func editPengeluaran(pax int) {
 	}
 }
 
+func hapusPengeluaran(A tabPengguna, B *tabkategori, C *tabPengeluaran, n int) {
+	var i, j, IDUser int
+	var pilihan int
+
+	fmt.Println()
+	fmt.Println("Hapus Pengeluaran : ")
+	fmt.Printf("Masukkan ID : ")
+	fmt.Scan(&IDUser)
+	fmt.Printf("Masukkan Kategori : (1. Akomodasi, 2. Transportasi, 3. Makanan, 4. Hiburan) : ")
+	fmt.Scan(&pilihan)
+
+	i = 0
+
+	for i < n && IDUser != A[i].id {
+		i++
+	}
+
+	j = pilihan - 1
+
+	if j >= 0 && j < 4 && i < n {
+		fmt.Printf("Berhasil mencatat untuk %s di kategori %s\n", A[i].nama, (*B)[j])
+	} else {
+		fmt.Println("Input tidak valid!")
+	}
+
+	(*C)[j] = 0
+	fmt.Printf("Berhasil menghapus pengeluaran pada %s ", (*B)[j])
+	fmt.Println(*C)
+
+}
+
 // KURANG INI PENAMAAN VARIABEL KURANG MIX N MATCH WKWKWKKWWK

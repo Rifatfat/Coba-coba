a5ea1a5c16fda47f4ce376c9ac7c68d2649ac6a5
tiarafitr <tiara.fitriannisha25@gmail.com>
2025-05-19 00:14:54 +0700

    yey

--- cobaacobaa.go
+++ cobaacobaa.go
@@ -1,5 +1,57 @@
 package main
 
-func main() n {
+import "fmt"
+
+type pengguna struct {
+	nama string
+	id   int
+}
+type Pengeluaran struct {
+	akomodasi    int
+	transportasi int
+	makanan      int
+	hiburan      int
+}
+
+const PAX int = 10
+
+type tabPengeluaran [PAX]Pengeluaran
+type tabPengguna [PAX]pengguna
+
+func main() {
+	var pax, budget, opsi int
+	var arrayUser tabPengguna
+	var arrayPengeluaran tabPengeluaran
+
+	fmt.Printf("Masukkan Jumlah Pengguna : ")
+	fmt.Scan(&pax)
+	fmt.Printf("Masukkan Budget Perjalanan : ")
+	fmt.Scan(&budget)
+	inputData(&arrayUser, &pax)
+	for {
+		tampilanMenu()
+		fmt.Scan(&opsi)
+
+		switch opsi {
+		case 1:
+			tambahPengeluaran(arrayUser, &arrayPengeluaran, pax)
+		case 2:
+			editPengeluaran(arrayUser, arrayPengeluaran, pax)
+
+		case 3:
+			hapusPengeluaran(arrayUser, &arrayPengeluaran, pax)
+		case 4:
+			lihatSemua(arrayUser, arrayPengeluaran, pax)
+		case 5:
+			cariPengeluaranSeq(arrayUser, arrayPengeluaran, pax)
+		case 7:
+			laporanAkhir(arrayUser, arrayPengeluaran, pax, budget)
+		}
+		if opsi == 8 {
+			fmt.Println("terimaksih ya!")
+			break
+		}
+
+	}
 
 }
